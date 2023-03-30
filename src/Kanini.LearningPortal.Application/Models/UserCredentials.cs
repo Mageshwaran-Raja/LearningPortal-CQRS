5602a3b26a2ffd5ddda9347494b39a028cfc5448
mageshwaran16 <mageshwaran.raja@kanini.com>
2023-03-30 11:38:59 +0530

    Models Added

---
+++ src/Kanini.LearningPortal.Application/Models/UserCredentials.cs
@@ -0,0 +1,8 @@
+﻿namespace Kanini.LearningPortal.Application.Models
+{
+    public class UserCredentials
+    {
+        public string MailId { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
